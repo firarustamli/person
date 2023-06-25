f5ce629cc5dd74557bff658f6d1251b9aa21f324
firarustamli <104448536+firarustamli@users.noreply.github.com>
2023-06-25 18:34:24 +0400

    Add files via upload

---
+++ Person.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace person
+
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+        public string Weight { get; set; }
+        public string Height { get; set; }
+
+        public override string ToString()
+        {
+            string v = $"Name: {Name}, Surname: {Surname}, Age: {Age}, Weight: {Weight}, Height: {Height}" ;
+ return v;
+}
+
+        class MainClass
+        {
+            public static void Main(string[] args)
+            {
+                Person person = new Person
+                {
+
+                    Name = "FIRA",
+                    Surname = "RUSTAMLI",
+                    Age = 25,
+                    Weight = "51",
+                    Height = "165"
+                };
+
+                string personString = person.ToString();
+                Console.WriteLine(personString);
+            }
+        }
+    }
+}
